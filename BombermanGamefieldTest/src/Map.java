60bf43ec44148233ae095c1e8e10deb3d7c8965c
Ant98 <anbit@gmx.de>
2017-01-09 00:01:25 +0100

    Add files via upload

--- BombermanGamefieldTest/src/Map.java
+++ BombermanGamefieldTest/src/Map.java
@@ -4,12 +4,19 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 /**
  * @author panos
  */
 public class Map extends BasicGame
 {
+	public static Input in;
+	public static int x, y;
+	
+	private static int heightScreen = 704;
+	private static int widhtScreen = 704;
+	
 	private Image emptyField = null;
 	private Image solidWall = null;
 	private Image destroyableWall = null;
@@ -17,9 +24,10 @@ public class Map extends BasicGame
 	private Image greenBomberman = null;
 	private Image redBomberman = null;
 	private Image yellowBomberman = null;
+	private Image bomb_phase1 = null;
 	
 	String GameMap[][] = {{"SW", "SW", "SW", "SW", "SW", "SW", "SW", "SW", "SW", "SW", "SW"},
-						{"SW", "BB", "EF", "DW", "DW", "DW", "DW", "DW", "EF", "RB", "SW"},
+						{"SW", "EF", "EF", "DW", "DW", "DW", "DW", "DW", "EF", "RB", "SW"},
 						{"SW", "EF", "SW", "DW", "SW", "DW", "SW", "DW", "SW", "EF", "SW"},
 						{"SW", "DW", "DW", "DW", "DW", "DW", "DW", "DW", "DW", "DW", "SW"},
 						{"SW", "DW", "SW", "DW", "SW", "DW", "SW", "DW", "SW", "DW", "SW"},
@@ -39,7 +47,10 @@ public class Map extends BasicGame
         try
         {
             AppGameContainer app = new AppGameContainer(new Map());
-            app.setDisplayMode(1000, 1000, false);
+            app.setDisplayMode(heightScreen, widhtScreen, false); // changed 1000 -> field * 64
+            app.setTargetFrameRate(60);	// inserted
+            app.setShowFPS(false);
+            app.setVSync(true);
             app.setAlwaysRender(true);
             app.start();
             
@@ -54,58 +65,119 @@ public class Map extends BasicGame
     @Override
     public void init(GameContainer container) throws SlickException
     {
-    	emptyField = new Image("data/empty_field.png");
-    	solidWall = new Image("data/wall_solid.png");
-    	destroyableWall = new Image("data/wall_destroyable.png");
-    	blueBomberman = new Image ("data/bomberman_blue.png");
-    	greenBomberman = new Image ("data/bomberman_green.png");
-    	redBomberman = new Image ("data/bomberman_rot.png");
-    	yellowBomberman = new Image ("data/bomberman_yellow.png");
+    	in = container.getInput();
+    	x=96;
+    	y=96;
     	
+    	emptyField = new Image("/data/empty_field.png");	//changed from data/... to /data/...
+    	solidWall = new Image("/data/wall_solid.png");	// and so on, change if there are problems
+    	destroyableWall = new Image("/data/wall_destroyable.png");	//with open the screen
+    	blueBomberman = new Image ("/data/bomberman_blue.png");
+    	greenBomberman = new Image ("/data/bomberman_green.png");
+    	redBomberman = new Image ("/data/bomberman_rot.png");
+    	yellowBomberman = new Image ("/data/bomberman_yellow.png");
+    	bomb_phase1 = new Image ("/data/bomb_phase1.png");
     }
  
-    @Override
     public void update(GameContainer container, int delta) throws SlickException
     {
+    	//key events
+    	//movement
+    	if(in.isKeyDown(in.KEY_W))	//possible also isKeyPressed
+    	{
+    		if(x%32==0 && x%64!=0)
+    		{
+    			if((GameMap[y/64-1][x/64]!="SW") && (GameMap[y/64-1][x/64]!="DW"))
+    					y-=4;
+    			else if(y%64!=32)
+    				y-=4;
+    		}
+    		
+    	}
+    	if(in.isKeyDown(in.KEY_S))
+    	{
+    		if(x%32==0 && x%64!=0)
+    		{
+    			if((GameMap[y/64+1][x/64]!="SW") && (GameMap[y/64+1][x/64]!="DW"))
+    					y+=4;
+    			else if(y%64!=32)
+    				y+=4;
+    		}
+    	}
+    	if(in.isKeyDown(in.KEY_A))
+    	{
+    		if(y%32==0 && y%64!=0)
+    		{
+    			if((GameMap[y/64][x/64-1]!="SW") && (GameMap[y/64][x/64-1]!="DW"))
+    					x-=4;
+    			else if(x%64!=32)
+    				x-=4;
+    		}
+    	}
+    	
+    	if(in.isKeyDown(in.KEY_D))
+    	{
+    		if(y%32==0 && y%64!=0)
+    		{
+    			if((GameMap[y/64][x/64+1]!="SW") && (GameMap[y/64][x/64+1]!="DW"))
+    					x+=4;
+    			else if(x%64!=32)
+    				x+=4;
+    		}
+    	}
+    	
+    	if(in.isKeyPressed(in.KEY_B))
+    		GameMap[y/64][x/64] = "BP1";
     	
     }
  
     public void render(GameContainer container, Graphics g) throws SlickException
-    {
+    {   	
     	for (int i=0; i<=10; i++)
     	{
     		for (int j=0; j<=10; j++)
     		{
     			if (GameMap[j][i] == "EF")
     			{
-    				emptyField.draw(i*64, j*64); 
+    				g.drawImage(emptyField, i*64, j*64);
+    				//emptyField.draw(i*64, j*64); 
     			}
     			if (GameMap[j][i] == "SW")
     			{
-    				solidWall.draw(i*64, j*64);
+    				g.drawImage(solidWall, i*64, j*64);
+    				//solidWall.draw(i*64, j*64);
     			}
     			if (GameMap[j][i] == "DW")
     			{
-    				destroyableWall.draw(i*64, j*64);
+    				g.drawImage(destroyableWall, i*64, j*64);
+    				//destroyableWall.draw(i*64, j*64);
     			}
     			if (GameMap[j][i] == "BB")
     			{
-    				blueBomberman.draw(i*64, j*64);
+    				g.drawImage(blueBomberman, i*64, j*64);
+    				//blueBomberman.draw(i*64, j*64);
     			}
     			if (GameMap[j][i] == "GB")
     			{
-    				greenBomberman.draw(i*64, j*64);
+    				g.drawImage(greenBomberman, i*64, j*64);
+    				//greenBomberman.draw(i*64, j*64);
     			}
     			if (GameMap[j][i] == "RB")
     			{
-    				redBomberman.draw(i*64, j*64);
+    				g.drawImage(redBomberman, i*64, j*64);
+    				//redBomberman.draw(i*64, j*64);
     			}
     			if (GameMap[j][i] == "YB")
     			{
-    				yellowBomberman.draw(i*64, j*64);
+    				g.drawImage(yellowBomberman, i*64, j*64);
+    				//yellowBomberman.draw(i*64, j*64);
     			}
-    		}	
+    			if (GameMap[j][i] == "BP1")
+    				g.drawImage(bomb_phase1, i*64, j*64);
+    		}
     	}
+    	blueBomberman.drawCentered(x, y);
+    	//g.drawImage(blueBomberman, x, y);
     	
     }
 }

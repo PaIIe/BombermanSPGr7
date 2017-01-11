a12f0b0b144077bedd11cb0d316f5af0a483d7bc
paiie <dannyhofmann93@web.de>
2017-01-11 22:34:48 +0100

    Added Junit test Boni

---
+++ BombermanGamefieldTest/src/tests/BoniTest.java
@@ -0,0 +1,60 @@
+package tests;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+/*import resolve problem..TODO*/
+import Bomberman;
+import Boni;
+import GameObject;
+
+/*Boni ist noch nicht funktional implementiert und wird im näcshten Sprint erst implementiert*/
+
+public class BoniSetMaxBombsTest 
+{
+	private static Boni boni;
+	private static Bomberman bomberman;
+	
+	@BeforeClass
+	
+	public static void bombermanHitBoni()
+	{
+		GameObject.Bomberman.column = GameObject.Boni.column;
+		GameObject.Bomberman.row = GameObject.Boni.row;
+		
+		if(Boni.checkPickUp() == true)
+			System.out.println("Stepped on the Boni");
+		else
+			System.out.println("Failed to Initialize Test: BoniSetMaxBombsTest ");
+
+	}
+	
+	
+	@Test
+	public void setMaxBombs()
+	{
+		int temp = bomberman.getMaxBombs();
+		boni.setMaxBombs();
+		assertEquals(bomberman.getMaxBombs(), temp + 1);
+	}
+	
+	@Test
+	public void setArmor()
+	{
+		boni.setArmor();
+		assertEquals(bomberman.getArmor, true);
+	}
+	
+	@Test
+	public void setBombExplosionSize()
+	{
+		int temp = bomberman.getExplosionSize()
+		boni.setBombExplosionSize();
+		assertEquals(bomberman.setBombExplosionSize, temp + 1);
+	}
+	
+	
+	
+
+
+}

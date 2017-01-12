cde2f7162df566d05d30ec8734e0d0b46af470d9
Willy <Willy@Willy-PC.fritz.box>
2017-01-12 13:12:37 +0100

    comment fix

--- BombermanGamefieldTest/src/tests/DestroyTest.java
+++ BombermanGamefieldTest/src/tests/DestroyTest.java
@@ -13,8 +13,8 @@ public class DestroyTest
 	@Test
 	public void BombDestroysWall(int row, int column, int time, int radius)
 	{
-		int x = 0;
-		int y = 0;
+		int x = 0; // value changed in explode() when wall is found
+		int y = 0; // value changed in explode() when wall is found
 		Bomb bomb = new Bomb(row, column, time, radius);
 		bomb.explode(); // explode function triggers destroyWall function if wall is in explosion radius with parameters(x, y) "destroyWall(x, y)" for location in matrix
 		assertEquals("EF", GameField.getMatrixElement(x, y));

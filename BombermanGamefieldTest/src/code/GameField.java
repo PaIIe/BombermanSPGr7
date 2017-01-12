b4f9cdca0c33b4c41a7ce14ea43c79b638f0a0d7
Willy <Willy@Willy-PC.fritz.box>
2017-01-12 11:52:16 +0100

    finished Test for Bomb destroys Wall

--- BombermanGamefieldTest/src/code/GameField.java
+++ BombermanGamefieldTest/src/code/GameField.java
@@ -8,4 +8,12 @@ public class GameField {
 	public void generateMap(){};
 	public int getHeight(){return 1;};
 	public int getWidth(){return 1;};
+	public String[][] getMatrix()
+	{
+		return this.matrix;
+	}
+	public String getMatrixElement(int row, int column)
+	{
+		return this.matrix[row][column];
+	}
 }
--- BombermanGamefieldTest/src/tests/DestroyTest.java
+++ BombermanGamefieldTest/src/tests/DestroyTest.java
@@ -4,17 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import Bomb;
-import Wall;
+import code.Bomb;
+import code.Wall;
+import code.GameField;
 
 public class DestroyTest 
 {
 	@Test
 	public void BombDestroysWall()
 	{
-		Bomb bomb = new Bomb();
-		bomb.explode(); // explode function triggers 
-		
+		Bomb bomb = new Bomb(row, column, time, radius);
+		bomb.explode(); // explode function triggers destroyWall function if wall is in explosion radius with parameters(x, y) "destroyWall(x, y)" for location in matrix
+		assertEquals("EF", GameField.getMatrixElement(x, y));
+
 	}
 	
 }

6b58225ba1e745d98c49b58017f926faf81704e9
piel923 <piel923@yahoo.com>
2017-03-30 19:06:58 +0200

    Add files via upload

---
+++ BombermanGameServer/src/LogsTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import code.Logs;
+
+/**
+ *
+ * @author bestx
+ */
+public class LogsTest {
+    
+    public LogsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    
+
+
+
+     @Test
+     public void BombLogTest()
+     {
+         assertEquals(true);
+     }
+     
+      @Test
+     public void MoveLogTest()
+     {
+         assertEquals(true);
+     }
+     
+      @Test
+     public void DiedLogTest()
+     {
+         assertEquals(true);
+     }
+     
+      @Test
+     public void LostArmorLogTest()
+     {
+         assertEquals(true);
+     }
+     
+      @Test
+     public void ExplodeLogTest()
+     {
+         assertEquals(true);
+     }
+     
+      @Test
+     public void DestroyedWallLogTest()
+     {
+         assertEquals(true);
+     }
+     
+      @Test
+     public void PowerUpLogTest()
+     {
+         assertEquals(true);
+     }
+    
+}
